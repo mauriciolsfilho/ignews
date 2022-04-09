cd5c82bd11609c174cd9bafe1432e4d3f2593699
Maurício Linhares <mauricio_1souza@outlook.com>
2022-04-09 20:38:50 -0300

    feat: :sparkles: estruturando a aplicação

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>ig.news | Home</title>
+      </Head>
+      <div style={{ padding: "2rem" }}>
+        <h2>Content</h2>
+      </div>
+    </>
+  );
+}
