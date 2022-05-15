01b1961a9328936615d4790d99f1cb141ac3a196
Maurício Linhares <mauricio_1souza@outlook.com>
2022-05-15 20:56:51 -0300

    feat: :sparkles: página de post.

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,8 +1,72 @@
-export default function Post() {
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/react";
+import * as prismic from "@prismicio/client";
+import { getPrismicClient } from "../../services/prismic";
+import { RichText } from "prismic-dom";
+import Head from "next/head";
+import { useEffect } from "react";
+
+import styles from "./post.module.scss";
+
+interface PostProps {
+  slug: string;
+  title: string;
+  content: string;
+  updatedAt: string;
+}
+export default function Post(props: PostProps) {
+  useEffect(() => {
+    if (props) {
+      console.log(props.content);
+      document.getElementById("content-post").innerHTML = props.content;
+    }
+  }, []);
+
   return (
     <>
-      <h1>TESTE</h1>
-      <p>TESTANDOOO</p>
+      <Head>
+        <title>{props.title} | ignews</title>
+      </Head>
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{props.title}</h1>
+          <time>{props.updatedAt}</time>
+
+          <div id="content-post" className={styles.content}></div>
+        </article>
+      </main>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  params,
+}) => {
+  const session = await getSession({ req });
+  const { slug } = params;
+
+  const prismic = getPrismicClient();
+  const response = await prismic.getByUID("post", String(slug), {});
+
+  const post = {
+    slug,
+    title: RichText.asText(response.data.title),
+    content: RichText.asHtml(response.data.content).replace(
+      " block-img",
+      "block-img"
+    ),
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString(
+      "pt-BR",
+      {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }
+    ),
+  };
+
+  return {
+    props: post,
+  };
+};
